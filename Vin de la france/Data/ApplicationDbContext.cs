2bb93850f382afc37cb7048254b4b22ebbea2b9c
ACLAIN <amaury.clain@viacesi.fr>
2024-08-30 09:31:54 +0200

    Ajoutez des fichiers projet.

---
+++ Vin de la france/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Vin_de_la_france.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Vin de la france/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vin_de_la_france.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
