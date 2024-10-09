94ddb0b387fcffc3b8a58093a6dd997644e6b597
ACLAIN <amaury.clain@viacesi.fr>
2024-10-09 11:34:46 +0200

    Déplacer vindelafronce2

---
+++ Vin_de_la_france_2/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vin_de_la_france_2.Models;
+
+namespace Vin_de_la_france_2.Data // Assurez-vous que l'espace de noms est correct
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // DbSets
+        public DbSet<ArticlesClass> ArticlesClasses { get; set; }
+        public DbSet<FamillesClass> FamillesClasses { get; set; }
+        public DbSet<FournisseursClass> FournisseursClasses { get; set; }
+    }
+}
---
+++ Vin_de_la_france_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vin_de_la_france_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
