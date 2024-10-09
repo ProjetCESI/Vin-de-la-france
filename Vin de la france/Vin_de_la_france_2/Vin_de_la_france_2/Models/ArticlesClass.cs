c62eaa9fd4cf511965c553ce93d10c4418e24e35
ACLAIN <amaury.clain@viacesi.fr>
2024-10-09 10:14:13 +0200

    Fin catalogue

---
+++ Vin de la france/Vin_de_la_france_2/Vin_de_la_france_2/Models/ArticlesClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vin_de_la_france_2.Models
+{
+    [Table("ArticlesClass")]
+    public class ArticlesClass
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int UnitPrice { get; set; }
+        public int QuantityStock { get; set; }
+        public int MinimumThreshold { get; set; }
+        public Guid Reference { get; set; }
+
+        public int FamillesClassId { get; set; }
+        public int FournisseursClassId { get; set; }
+
+        // Relations avec les autres entités
+        public virtual FamillesClass FamillesClass { get; set; }
+        public virtual FournisseursClass FournisseursClass { get; set; }
+    }
+}
---
+++ Vin de la france/Vin_de_la_france_2/Vin_de_la_france_2/Models/FournisseursClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Vin_de_la_france_2.Models;
+using System.ComponentModel.DataAnnotations.Schema;  // Ajoutez cette directive pour l'attribut [Table]
+
+
+namespace Vin_de_la_france_2.Models
+{
+    [Table("FournisseursClass")]
+    public class FournisseursClass
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<ArticlesClass> ArticlesClasses { get; set; }
+    }
+}
